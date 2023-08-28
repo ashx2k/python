0ffb4122e31afb01319b5e0ff51032b51ff8bd0a
Ashx2k <akash20012000@gmail.com>
2023-08-28 11:23:57 +0530

    Finding valid Age

---
+++ agevalid.py
@@ -0,0 +1,6 @@
+name = input("Enter your name : " )
+age = int(input("Enter Your Age : "))
+if age>=18:
+    print (name + " your age is : " , age , " so you are Eligible to vote")
+else:
+    print (name + " your age is : " , age , " so you are Not-Eligible to vote")
