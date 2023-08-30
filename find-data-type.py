5cf4b85e06c2b4a93029910270a4af84df05281f
Ashx2k <akash20012000@gmail.com>
2023-08-30 12:56:47 +0530

    printing data type

---
+++ find-data-type.py
@@ -0,0 +1,15 @@
+a = 1
+print(type(a))
+b = "hello"
+print(type(b))
+c = True
+print(type(c))
+d = [1,2,3,4,5]
+print(type(d))
+e = (1,2,3,4,5)
+print(type(e))
+f = {"name" : "rob" ,"age" : 23}
+print(type(f))
+g = {1,2,3}
+print(type(g))
+
